4772fa07bfeb7ccadf209d6bab053f99a67939a9
LIShuning <lishiuning1955142623@outlook.com>
2019-06-28 11:47:35 +0800

    feat: get employee list

---
+++ src/main/java/com/example/employee/controllers/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.example.employee.controllers;
+
+import com.example.employee.entity.Employee;
+import com.example.employee.repopsitory.EmployeeRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+    private EmployeeRepository repository;
+
+    public EmployeeController() {
+        this.repository = new EmployeeRepository();
+    }
+
+    @RequestMapping(value = "/employees", method = RequestMethod.GET)
+    public List<Employee> getAll() {
+        return repository.findAll();
+    }
+}
