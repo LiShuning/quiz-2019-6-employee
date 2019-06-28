3bad1eb9557dd60ba5b4ee601ea901d4dcd9c11a
LIShuning <lishiuning1955142623@outlook.com>
2019-06-28 11:48:09 +0800

    feat: get employee detail

--- src/main/java/com/example/employee/controllers/EmployeeController.java
+++ src/main/java/com/example/employee/controllers/EmployeeController.java
@@ -2,6 +2,7 @@ package com.example.employee.controllers;
 
 import com.example.employee.entity.Employee;
 import com.example.employee.repopsitory.EmployeeRepository;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,5 +20,10 @@ public class EmployeeController {
     @RequestMapping(value = "/employees", method = RequestMethod.GET)
     public List<Employee> getAll() {
         return repository.findAll();
+    }
+
+    @RequestMapping(value = "/employees/{id}" , method = RequestMethod.GET)
+    public Employee getById(@PathVariable String id) {
+        return repository.findById(Integer.valueOf(id));
     }
 }
